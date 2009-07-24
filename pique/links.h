496279511031ab2ced6ffc70b00839152a024a83
colin.rafferty <colin.rafferty@3c440e1a-f302-11dd-a320-db7d8e7a638f>
2009-07-24 04:16:02 +0000

    new board members, better links in pique

--- pique/links.h
+++ pique/links.h
@@ -6,42 +6,21 @@
 <li><a href="authors.html">Index By Author</a>
 <li><a href="subjects.html">Index By Subject</a>
 
-<h3>Pique 2004</h3>
-
-<li><a href="2004-12.html">December 2004</a>
-<li><a href="2004-11.html">November 2004</a>
-<li><a href="2004-10.html">October 2004</a>
-<li><a href="2004-09.html">September 2004</a>
-<li><a href="2004-07.html">July 2004</a>
-<li><a href="2004-06.html">June 2004</a>
-<li><a href="2004-05.html">May 2004</a>
-<li><a href="2004-04.html">April 2004</a>
-<li><a href="2004-03.html">March 2004</a>
-<li><a href="2004-02.html">February 2004</a>
-<li><a href="2004-01.html">January 2004</a>
-
-<h3>Pique 2003</h3>
-<li><a href="2003-12.html">December 2003</a>
-<li><a href="2003-11.html">November 2003</a>
-<li><a href="2003-10.html">October 2003</a>
-<li><a href="2003-09.html">September 2003</a>
-<li><a href="2003-07.html">July 2003</a>
-<li><a href="2003-06.html">June 2003</a>
-<li><a href="2003-05.html">May 2003</a>
-<li><a href="2003-04.html">April 2003</a>
-<li><a href="2003-03.html">March 2003</a>
-<li><a href="2003-02.html">February 2003</a>
-<li><a href="2003-01.html">January 2003</a>
-
-<h3>Pique 2002</h3>
-<li><a href="2002-12.html">December 2002</a>
-<li><a href="2002-11.html">November 2002</a>
-<li><a href="2002-10.html">October 2002</a>
-<li><a href="2002-09.html">September 2002</a>
-<li><a href="2002-07.html">July 2002</a>
-<li><a href="2002-06.html">June 2002</a>
-<li><a href="2002-05.html">May 2002</a>
-<li><a href="2002-04.html">April 2002</a>
-<li><a href="2002-03.html">March 2002</a>
-<li><a href="2002-02.html">February 2002</a>
-<li><a href="2002-01.html">January 2002</a>
+<h3>Other Sites</h3>
+<li><a href="http://www.secularhumanism.org/">Council for Secular Humanism</a>
+<li><a href="http://www.cfimetrony.org/">CFI-MetroNY</a>
+<li><a href="http://nyhumanist.org/cfi_long_island.htm">Long Island Secular Humanists</a>
+<li><a href="http://w.humanistnews.blogspot.com/">Humanist News Weblog</a>
+<li><a href="http://www.humaniststudies.org/">Institute for Humanist Studies</a>
+<li><a href="http://www.au.org/">Americans United for Separation of Church and State</a>
+<li><a href="http://www.atheists.org/">American Atheists</a>
+<li><a href="http://nyc-atheists.org/">NYC Atheists</a>
+<li><a href="http://www.americanhumanist.org/">American Humanist Association</a>
+<li><a href="http://www.freethoughtaction.org/">Free Thought Action</a>
+<li><a href="http://www.infidelguy.com/">The Infidel Guy</a>
+<li><a href="http://www.rationallyspeaking.org/">Rationally Speaking</a>
+<li><a href="http://the-brights.net/">The Brights</a>
+<li><a href="http://www.centerforinquiry.net/">Center for Inquiry</a>
+<li><a href="http://www.csicop.org/">Committee for the Scientific Investigation of Claims of the Paranormal</a>
+<li><a href="http://dir.yahoo.com/Arts/Humanities/Philosophy/Humanism">Yahoo Humanism Index</a>
+<li><a href="http://dir.yahoo.com/Society_and_Culture/Religion_and_Spirituality/Faiths_and_Practices/Atheism/">Yahoo Atheism Index</a>
