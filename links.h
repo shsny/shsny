b463e4cfe6b6a449122f0fc87a69d37085fe5cf5
colin.rafferty <colin.rafferty@3c440e1a-f302-11dd-a320-db7d8e7a638f>
2010-09-14 02:09:53 +0000

    donate

--- links.h
+++ links.h
@@ -1,4 +1,6 @@
 <!-- -*- html -*- -->
+<h3>Donate Now</h3>
+<li> <form action="https://www.paypal.com/cgi-bin/webscr" method="post"> <input type="hidden" name="cmd" value="_s-xclick"> <input type="hidden" name="hosted_button_id" value="4CMJGNLMKMJEG"> <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> </form>
 <h3>Other Sites</h3>
 <li><a href="http://www.secularhumanism.org/">Council for Secular Humanism</a>
 <li><a href="http://www.centerforinquiry.net/nyc">Center for Inquiry - New York City</a>
