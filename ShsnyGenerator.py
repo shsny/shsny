e7c71ba367cf4bfe349240e285f7c817687e228f
colin@rafferty.net <colin@rafferty.net@3c440e1a-f302-11dd-a320-db7d8e7a638f>
2013-04-03 03:09:28 +0000

    proper google analytics tracking

--- ShsnyGenerator.py
+++ ShsnyGenerator.py
@@ -76,17 +76,7 @@ class ShsnyGenerator(Skeleton, Sidebar, Banner):
 
     def get_body(self):
         if self.__body is None:
-            self.__body = self.__parser.fp.read() + """\
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-39819106-1', 'shsny.org');
-ga('send', 'pageview');
-
-</script>"""
+            self.__body = self.__parser.fp.read()
         return self.__body
 
 # end ShsnyGenerator.py
--- ht2html-2.0/Skeleton.py
+++ ht2html-2.0/Skeleton.py
@@ -288,6 +288,17 @@ Title: %(title)s
 <meta http-equiv="Content-Type" content="text/html; charset=%(charset)s">
 %(meta)s
 %(style)s
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39819106-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>''' % {'title'   : self.get_title(),
               'headers' : self.get_headers(),
               'meta'    : self.get_meta(),
