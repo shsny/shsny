c00ef58f3963f61be1ad1c0bf3667f6abb35eeae
colin@rafferty.net <colin@rafferty.net@3c440e1a-f302-11dd-a320-db7d8e7a638f>
2013-11-26 02:12:30 +0000

    update links

--- links.h
+++ links.h
@@ -2,6 +2,10 @@
 <h3>Donate Now</h3>
 <li> <form action="https://www.paypal.com/cgi-bin/webscr" method="post"> <input type="hidden" name="cmd" value="_s-xclick"> <input type="hidden" name="hosted_button_id" value="4CMJGNLMKMJEG"> <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> </form>
 <h3>Other Sites</h3>
+
+<li><a href="http://states.secular.org/states/new-york">Secular Coalition for New York</a>
+<li><a href="https://www.facebook.com/SecularCoalitionNY">Secular Coalition for NY Community Page</a>
+<li><a href="http://secular.org/">Secular Coalition for America</a>
 <li><a href="http://www.secularhumanism.org/">Council for Secular Humanism</a>
 <li><a href="http://www.humanistinstitute.org/">The Humanist Institute</a>
 <li><a href="http://www.centerforinquiry.net/nyc">Center for Inquiry - New York City</a>
@@ -13,10 +17,7 @@
 <li><a href="http://www.americanhumanist.org/">American Humanist Association</a>
 <li><a href="http://citycongregation.org/">The City Congregation for Humanistic Judaism</a>
 <li><a href="http://www.freethoughtaction.org/">Free Thought Action</a>
-<li><a href="http://www.infidelguy.com/">The Infidel Guy</a>
 <li><a href="http://www.rationallyspeaking.org/">Rationally Speaking</a>
 <li><a href="http://the-brights.net/">The Brights</a>
 <li><a href="http://www.centerforinquiry.net/">Center for Inquiry</a>
 <li><a href="http://www.csicop.org/">Committee for the Scientific Investigation of Claims of the Paranormal</a>
-<li><a href="http://dir.yahoo.com/Arts/Humanities/Philosophy/Humanism">Yahoo Humanism Index</a>
-<li><a href="http://dir.yahoo.com/Society_and_Culture/Religion_and_Spirituality/Faiths_and_Practices/Atheism/">Yahoo Atheism Index</a>
