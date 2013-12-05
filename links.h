68b449c419d9c3a68c34f261edd04d7ee2cd0834
colin@rafferty.net <colin@rafferty.net@3c440e1a-f302-11dd-a320-db7d8e7a638f>
2013-12-05 00:34:21 +0000

    add affiliations

--- links.h
+++ links.h
@@ -3,13 +3,14 @@
 <li> <form action="https://www.paypal.com/cgi-bin/webscr" method="post"> <input type="hidden" name="cmd" value="_s-xclick"> <input type="hidden" name="hosted_button_id" value="4CMJGNLMKMJEG"> <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> </form>
 <h3>Other Sites</h3>
 
+<li><a href="http://www.secularhumanism.org/">Council for Secular Humanism</a>
+<li><a href="http://www.atheistalliance.org/">Atheist Alliance International</a>
+<li><a href="http://www.americanhumanist.org/">American Humanist Association</a>
 <li><a href="http://states.secular.org/states/new-york">Secular Coalition for New York</a>
 <li><a href="https://www.facebook.com/SecularCoalitionNY">Secular Coalition for NY Community Page</a>
 <li><a href="http://secular.org/">Secular Coalition for America</a>
-<li><a href="http://www.atheistalliance.org/">Atheist Alliance International</a>
 <li><a href="http://www.reasonablenewyork.org/">Reasonable New York</a>
 <li><a href="http://www.feministfreethinkers.org/">Feminist Freethinkers of New York</a>
-<li><a href="http://www.secularhumanism.org/">Council for Secular Humanism</a>
 <li><a href="http://www.humanistinstitute.org/">The Humanist Institute</a>
 <li><a href="http://www.centerforinquiry.net/nyc">Center for Inquiry - New York City</a>
 <li><a href="http://www.centerforinquiry.net/li">Center for Inquiry - Long Island</a>
@@ -17,7 +18,6 @@
 <li><a href="http://www.au.org/">Americans United for Separation of Church and State</a>
 <li><a href="http://www.atheists.org/">American Atheists</a>
 <li><a href="http://nyc-atheists.org/">NYC Atheists</a>
-<li><a href="http://www.americanhumanist.org/">American Humanist Association</a>
 <li><a href="http://citycongregation.org/">The City Congregation for Humanistic Judaism</a>
 <li><a href="http://www.freethoughtaction.org/">Free Thought Action</a>
 <li><a href="http://www.rationallyspeaking.org/">Rationally Speaking</a>
